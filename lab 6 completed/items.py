d1406d437c52b21cd02164bc57c4b41511c14190
EmmaHall1 <e.hall02@live.co.uk>
2017-10-19 15:01:09 +0100

    Lab 6 completed

---
+++ lab 6 completed/items.py
@@ -0,0 +1,66 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?...""",
+    "mass": 200
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!",
+    "mass": 2000
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees.",
+    "mass": 200
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "description": "A pack of biscuits.",
+    "mass": 500
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    "description": "A basic ballpoint pen.",
+    "mass": 1000
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+
+    "description": "This student handbook explains everything. Seriously.",
+    "mass": 1000
+}
+
+every_item = {
+    "id": item_id,
+    "laptop": item_laptop,
+    "money": item_money,
+    "biscuits": item_biscuits,
+    "pen": item_pen,
+    "handbook": item_handbook,
+}
